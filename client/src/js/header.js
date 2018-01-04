694b8cc1aee9015142ba7a07abd8cb876fd9b827
Dattu Kumar <dattu.kumar@mountblue.io>
2018-01-04 13:26:45 +0530

    created App Bar

--- client/src/js/header.js
+++ client/src/js/header.js
@@ -3,23 +3,38 @@ import ReactDOM from 'react-dom';
 import AppBar from 'material-ui/AppBar';
 import Toolbar from 'material-ui/Toolbar';
 import IconButton from 'material-ui/IconButton';
-import MenuIcon from 'material-ui-icons/Menu'
-import Typography from 'material-ui/Typography'
-import Button from 'material-ui/Button'
+import MenuIcon from 'material-ui-icons/Menu';
+import Typography from 'material-ui/Typography';
+import Button from 'material-ui/Button';
+
+const styles = {
+  root: {
+    width: '100%',
+  },
+  flex: {
+    flex: 1,
+  },
+  menuButton: {
+    marginLeft: -12,
+    marginRight: 20,
+  },
+};
 
 export default class Header extends React.Component{
   render(){
     return(
-      <div>
+      <div style={{display:"flex"}}>
         <AppBar position="static">
         <Toolbar>
           <IconButton color="contrast" aria-label="Menu">
             <MenuIcon />
           </IconButton>
-          <Typography type="title" color="inherit">
-            Title
+          <Typography type="title" color="inherit" className={this.props.flex}>
+            CrytpX
           </Typography>
-          <Button color="contrast">Login</Button>
+          <div>
+            <Button color="contrast">Login</Button>
+          </div>
         </Toolbar>
       </AppBar>
       </div>

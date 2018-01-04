fab47d27888474c1437bd62ab38a5eb59c42450a
Shweta Mishra <shweta.mishra@mountblue.io>
2018-01-04 18:03:50 +0530

    Implement Login

--- client/src/js/login.jsx
+++ client/src/js/login.jsx
@@ -9,6 +9,7 @@ import Button from 'material-ui/Button';
 import { FormControl, FormHelperText } from 'material-ui/Form';
 import Input, { InputLabel, InputAdornment } from 'material-ui/Input';
 import Grid from 'material-ui/Grid';
+import OverridesClassNames from "./icon.js";
 
 const styles = theme => ({
   container: {
@@ -25,14 +26,16 @@ const styles = theme => ({
   },
   input: {
     display: 'block',
-  },
-
-
+  }
 })
+const icons = {
+  'google': 'M522.2 438.8v175.6h290.4c-11.8 75.4-87.8 220.8-290.4 220.8-174.8 0-317.4-144.8-317.4-323.2s142.6-323.2 317.4-323.2c99.4 0 166 42.4 204 79l139-133.8c-89.2-83.6-204.8-134-343-134-283 0-512 229-512 512s229 512 512 512c295.4 0 491.6-207.8 491.6-500.2 0-33.6-3.6-59.2-8-84.8l-483.6-0.2z'
+};
+
 
 function loginUser(){
   fetch("http://localhost:5000/auth/login",{
-    method:"POST",
+  // method:"POST",
     body:JSON.stringify({
       email:document.getElementById('email').value,
       password: document.getElementById('password').value
@@ -95,6 +98,7 @@ export  class Login extends Component {
           <hr className="hr-text" data-content="Or"></hr>
           <div id="signG">
             <Button type="submit" raised color="accent" className={classes.button} onClick={this.loginUser}>
+              <OverridesClassNames/>
               Continue with Google+
               </Button>
           </div>
@@ -106,21 +110,9 @@ export  class Login extends Component {
   }
 }
 
+
 Login.propTypes = {
   classes: PropTypes.object.isRequired,
 };
 
 export default withStyles(styles)(Login);
-
-// export default withStyles(styles)(Login);
-// class Form extends Component{
-//   // render(){
-//   //   return (
-      
-
-
-//   //     );
-//   // }
-// }
-
-// module.exports=Login;

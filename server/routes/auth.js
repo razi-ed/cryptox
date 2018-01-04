5a149c07d40ada61c92a36fc6d6d0e2dc15d7be3
akshay upadhya <akshay.upadhya@mountblue.io>
2018-01-04 13:38:27 +0530

    adding user to db encrypting his password

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -1,4 +1,7 @@
 const express = require('express');
+const expressValidator = require('express-validator');
+
+
 const router = express.Router();
 let login, register ,validate,resetPassword;
 login= require('./auth/login')
@@ -6,6 +9,7 @@ validate= require('./auth/validate')
 resetPassword= require('./auth/resetPassword')
 register= require('./auth/registerUser')
 
+router.use(expressValidator())
 router.post('/login',login)
 router.post('/register',register)
 router.get('/validate',validate)
--- server/routes/auth/registerUser.js
+++ server/routes/auth/registerUser.js
@@ -1,4 +1,27 @@
+const user= require('../../models/users')
+
 let registerUser =(req,res)=>{
-  res.send('registerUser')
+  let name=req.body.name
+  let email=req.body.email
+  let password=req.body.password
+  req.checkBody('name','Username is required').notEmpty()
+  req.checkBody('email','email is required').notEmpty()
+  req.checkBody('password','password is required').notEmpty()
+  let errors= req.validationErrors()
+  if(errors){
+    res.json(errors[0].msg)
+  }else{
+    var newUser = new user({
+      name,email,password
+      //Secret question is commented 
+		});
+
+		user.createUser(newUser, function(err, user){
+			if(err) throw err;
+      console.log(user);
+      res.json(user)
+		});
+  }
+  //res.json({name,email,password,errors})
 }
 module.exports=registerUser

0233145dad4cd5224a3a8e4dcb30a376434b956e
Neel Parmar <neel.parmar@mountblue.io>
2018-01-05 10:48:03 +0530

    refractror login react component code

--- client/src/js/login.jsx
+++ client/src/js/login.jsx
@@ -28,12 +28,6 @@ const styles = theme => ({
     display: 'block',
   }
 })
-const icons = {
-  'google': 'M522.2 438.8v175.6h290.4c-11.8 75.4-87.8 220.8-290.4 220.8-174.8 0-317.4-144.8-317.4-323.2s142.6-323.2 317.4-323.2c99.4 0 166 42.4 204 79l139-133.8c-89.2-83.6-204.8-134-343-134-283 0-512 229-512 512s229 512 512 512c295.4 0 491.6-207.8 491.6-500.2 0-33.6-3.6-59.2-8-84.8l-483.6-0.2z'
-};
-
-
-
 
 export  class Login extends Component {
   state = {
@@ -48,7 +42,7 @@ export  class Login extends Component {
       method:"POST",
       headers:{
         'Content-Type':'application/json',
-        },
+      },
       body:JSON.stringify({
         email:document.getElementById('log-email').value,
         password: document.getElementById('log-password').value
@@ -58,7 +52,6 @@ export  class Login extends Component {
     })
   }
   
-
   handleChange = prop => event => {
 
     this.setState({
@@ -106,7 +99,6 @@ export  class Login extends Component {
     );
   }
 }
-
 
 Login.propTypes = {
   classes: PropTypes.object.isRequired,

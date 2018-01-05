f454fec7c40eaf950c429de12e9b8cbbe9ba6583
Dattu Kumar <dattu.kumar@mountblue.io>
2018-01-05 17:48:35 +0530

    removed password validation

--- client/src/js/signup.js
+++ client/src/js/signup.js
@@ -32,46 +32,6 @@ export default class SignUp extends React.Component {
     });
   }
 
-  validatePassword(event) {
-    var lowerCaseLetters = /[a-z]/g;
-    if (event.target.value.match(lowerCaseLetters)) {
-      document.getElementById("letter").classList.remove("invalid");
-      document.getElementById("letter").classList.add("valid");
-    } else {
-      document.getElementById('letter').classList.remove("valid");
-      document.getElementById('letter').classList.add("invalid");
-    }
-
-    // Validate capital letters
-    var upperCaseLetters = /[A-Z]/g;
-    if (event.target.value.match(upperCaseLetters)) {
-      document.getElementById("capital").classList.remove("invalid");
-      document.getElementById("capital").classList.add("valid");
-    } else {
-      document.getElementById("capital").classList.remove("valid");
-      document.getElementById("capital").classList.add("invalid");
-    }
-
-    // Validate numbers
-    var numbers = /[0-9]/g;
-    if (event.target.value.match(numbers)) {
-      document.getElementById("number").classList.remove("invalid");
-      document.getElementById("number").classList.add("valid");
-    } else {
-      document.getElementById("number").classList.remove("valid");
-      document.getElementById("number").classList.add("invalid");
-    }
-
-    // Validate length
-    if (event.target.value.length >= 8) {
-      document.getElementById("length").classList.remove("invalid");
-      document.getElementById("length").classList.add("valid");
-    } else {
-      document.getElementById("length").classList.remove("valid");
-      document.getElementById("length").classList.add("invalid");
-    }
-  }
-
   handleClickShowPasssword(){
     this.setState({showPassword:!this.state.showPassword})
   }
@@ -79,21 +39,20 @@ export default class SignUp extends React.Component {
   render() {
     return (
       <div>
-        <Grid container>
-          <Grid item sm={4} xs={2} />
-          <Grid item sm={4} xs={8} style={{ alignItems: "center" }}>
           <div>
           <FormControl className="formElements" >
             <InputLabel>Name</InputLabel>
             <Input autoFocus="true" id="new-name" type="name"/>
           </FormControl>
           </div>
+          
           <div>
           <FormControl className="formElements" >
             <InputLabel >Email</InputLabel>
             <Input id="email" type="email"/>
           </FormControl>
           </div>
+          
           <div>
           <FormControl className="formElements" >
             <InputLabel>Password</InputLabel>
@@ -112,6 +71,7 @@ export default class SignUp extends React.Component {
               <FormHelperText>Password must have atleat 8 characters</FormHelperText>
           </FormControl>
           </div>
+          
           <div>
           <FormControl className="formElements" >
             <InputLabel>Re-Enter Password</InputLabel>
@@ -119,23 +79,6 @@ export default class SignUp extends React.Component {
              />
           </FormControl>
           </div>
-            
-              <div id="message" style={{ display: "none" }}>
-                <h3>Password must contain the following:</h3>
-                <p id="letter" className="invalid">
-                  A <b>lowercase</b> letter
-                </p>
-                <p id="capital" className="invalid">
-                  A <b>capital (uppercase)</b> letter
-                </p>
-                <p id="number" className="invalid">
-                  A <b>number</b>
-                </p>
-                <p id="length" className="invalid">
-                  Minimum <b>8 characters</b>
-                </p>
-              </div>
-
               <div style={{ paddingTop: 25, textAlign: "center" }}>
                 <Button
                   raised
@@ -146,11 +89,7 @@ export default class SignUp extends React.Component {
                   Create New Account
                 </Button>
               </div>
-          </Grid>
-
-          <Grid item sm={4} xs={6} />
-        </Grid>
-      </div>
+        </div>
     );
   }
 }

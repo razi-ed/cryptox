1fe7d497092e65ef23735ede7259fe76fcb21801
Dattu Kumar <dattu.kumar@mountblue.io>
2018-01-06 12:52:22 +0530

    created resposive form helper text

--- client/src/js/signup.js
+++ client/src/js/signup.js
@@ -10,30 +10,72 @@ import Visibility from 'material-ui-icons/Visibility'
 import VisibilityOff from 'material-ui-icons/VisibilityOff'
 import {InputAdornment} from "material-ui/Input";
 import IconButton from "material-ui/IconButton";
+import Dialog,{DialogActions, DialogContent, DialogContentText, DialogTitle,} from 'material-ui/Dialog'
+
 export default class SignUp extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
+      name:"",
+      email:"",
+      password:"",
       showPassword: false,
-      isUserCreated: false
+      isUserCreated: false,
+      isDialogOpen:false,
+      passwordColor:"red",
+      passwordHelperText:"none",
+      confirmPasswordColor:"red",
+      confirmHelperText:"none",
+      isPasswordMatch:false,
+      isPasswordSet:false,
     };
-    this.handleClickShowPasssword=this.handleClickShowPasssword.bind(this)
+    this.validatePassword=this.validatePassword.bind(this);
+    this.confirmPassword=this.confirmPassword.bind(this);
+    this.createUser=this.createUser.bind(this);
+    this.handleClickIcon=this.handleClickIcon.bind(this);
   }
   createUser() {
-    fetch("http://localhost:5000/auth/register", {
-      method: "POST",
-      body: JSON.stringify({
-        name: document.getElementById("name").value,
-        email: document.getElementById("email").value,
-        password: document.getElementById("password").value
-      })
-    }).then(res => {
-      console.log(res);
-    });
+    if(this.state.name && this.state.email && this.state.password){
+      fetch("/auth/register", {
+        method: "POST",
+        body: JSON.stringify({
+          name:this.state.name,
+          email: this.state.email,
+          password:this.state.password
+        })
+      }).then(res=>res.json()).then(res => {
+        console.log(res);
+      });
+    }
+    else{
+      this.setState({isDialogOpen:true})
+     
+    }
   }
 
-  handleClickShowPasssword(){
+  handleClickIcon(){
     this.setState({showPassword:!this.state.showPassword})
+  }
+
+  validatePassword(event){
+    const pattern=new RegExp(/\w{8,}/)
+    this.setState({helperTextVisible:"block"})
+    if(event.target.value.match(pattern)){
+      this.setState({password:event.target.value,passwordColor:"green",passwordHelperText:"block"},()=>console.log(this.state.password))
+    }
+    else{
+      this.setState({password:event.target.value,passwordColor:"red",passwordHelperText:"block"})
+    }
+  }
+
+  confirmPassword(event){
+    this.setState({confirmHelperText:"block"})
+    if(this.state.password==event.target.value){
+      this.setState({isPasswordMatch:true,confirmPasswordColor:"green",isPasswordSet:true,confirmHelperText:"block"})
+    }
+    else{
+      this.setState({isPasswordMatch:false,confirmPasswordColor:"red",isPasswordSet:false,confirmHelperText:"block"})
+    }
   }
 
   render() {
@@ -42,14 +84,20 @@ export default class SignUp extends React.Component {
           <div>
           <FormControl className="formElements" >
             <InputLabel>Name</InputLabel>
-            <Input autoFocus="true" id="new-name" type="name"/>
+            <Input 
+              autoFocus={true} 
+              onChange={(event)=>this.setState({name:event.target.value})}
+              />
           </FormControl>
           </div>
           
           <div>
           <FormControl className="formElements" >
             <InputLabel >Email</InputLabel>
-            <Input id="email" type="email"/>
+            <Input 
+              type="email"
+              onChange={(event)=>this.setState({email:event.target.value})}
+            />
           </FormControl>
           </div>
           
@@ -59,24 +107,30 @@ export default class SignUp extends React.Component {
             <Input
               id="password" 
               type={this.state.showPassword?'text':'password'}
+              onChange={this.validatePassword}
+              onFocus={this.validatePassword}
               endAdornment={
                 <InputAdornment position="end">
-                  <IconButton onClick={this.handleClickShowPasssword} >
+                  <IconButton onClick={this.handleClickIcon} >
                     {this.state.showPassword?<VisibilityOff/>:<Visibility/>}
                   </IconButton>
                 </InputAdornment>
 
               }  
               />
-              <FormHelperText>Password must have atleat 8 characters</FormHelperText>
+              <FormHelperText style={{color:this.state.passwordColor,display:this.state.passwordHelperText}} >Password length must be between 8 to 15 characters characters</FormHelperText>
           </FormControl>
           </div>
           
           <div>
           <FormControl className="formElements" >
             <InputLabel>Re-Enter Password</InputLabel>
-            <Input id="confirm" type="password"
+            <Input 
+              id="confirm" 
+              type="password"
+              onChange={this.confirmPassword}
              />
+            <FormHelperText style={{color:this.state.confirmPasswordColor,display:this.state.confirmHelperText}} >{this.state.isPasswordMatch?'Password Match':'Password Mismatch'}</FormHelperText>
           </FormControl>
           </div>
               <div style={{ paddingTop: 25, textAlign: "center" }}>

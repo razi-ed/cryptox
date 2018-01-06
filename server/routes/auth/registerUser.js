3cb2ddcaeac4ff7f3904e3a67260bf8e53958e4f
akshay upadhya <akshay.upadhya@mountblue.io>
2018-01-06 16:02:10 +0530

    backend email ,assword validation

--- server/routes/auth/registerUser.js
+++ server/routes/auth/registerUser.js
@@ -1,15 +1,17 @@
 const User= require('../../models/users')
-
+const validatePassword= require('../../../utils/ValidateUser')
 const registerUser =(req,res)=>{
   const name=req.body.name
   const email=req.body.email
   const password=req.body.password
   req.checkBody('name','Username is required').notEmpty()
-  req.checkBody('email','email is required').notEmpty()
+  req.checkBody('email','email is required').notEmpty().isemail()
   req.checkBody('password','password is required').notEmpty()
   const errors= req.validationErrors()
   if(errors){
     res.json(errors[0].msg)
+  }else if(validatePassword(password)!=="valid password"){
+    res.send("password "+validatePassword(password))
   }else{
     const newUser = new User({
       name,email,password

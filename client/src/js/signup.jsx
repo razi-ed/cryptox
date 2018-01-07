d15fdbf8cda18ed219d9988366914f28a576e0a6
Razi <razi.mathilakath@gmail.com>
2018-01-07 19:48:27 +0530

    Beta

--- client/src/js/signup.jsx
+++ client/src/js/signup.jsx
@@ -3,154 +3,151 @@ import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 import Button from "material-ui/Button";
 import Grid from "material-ui/Grid";
 import TextField from "material-ui/TextField";
-import Input,{InputLabel} from 'material-ui/Input'
-import {FormHelperText,FormControl} from 'material-ui/Form'
+import Input, { InputLabel } from 'material-ui/Input'
+import { FormHelperText, FormControl } from 'material-ui/Form'
 import Visibility from 'material-ui-icons/Visibility'
 import VisibilityOff from 'material-ui-icons/VisibilityOff'
-import {InputAdornment} from "material-ui/Input";
+import { InputAdornment } from "material-ui/Input";
 import IconButton from "material-ui/IconButton";
+import GoogleButton from 'react-google-button';
+
 export default class SignUp extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
+      name: "",
+      email: "",
+      password: "",
       showPassword: false,
-      isUserCreated: false
+      passwordColor: "red",
+      passwordHelperText: "none",
+      confirmPasswordColor: "red",
+      confirmHelperText: "none",
+      isPasswordMatch: false,
+      isPasswordSet: false,
     };
-    this.handleClickShowPasssword=this.handleClickShowPasssword.bind(this)
+    this.validatePassword = this.validatePassword.bind(this);
+    this.confirmPassword = this.confirmPassword.bind(this);
+    this.createUser = this.createUser.bind(this);
+    this.handleClickIcon = this.handleClickIcon.bind(this);
   }
-  
-  createUser(){
-    console.log(document.getElementById('name').value)
-    fetch("/auth/register",{
-      method:"POST",
-      headers:{
-      'Content-Type':'application/json',
-      },
-      body:JSON.stringify({
-        name:document.getElementById('name').value,
-      email:document.getElementById('email').value,
-      password:document.getElementById('password').value,
-      })
-    }).then(res=>res.json()).then((res)=>{
-      console.log(res);
-    });
+  createUser() {
+    console.log(this.state.name, this.state.email, this.state.password)
+    if (this.state.name && this.state.email && this.state.password) {
+      fetch("/auth/register", {
+        method: "POST",
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+          name: this.state.name,
+          email: this.state.email,
+          password: this.state.password
+        })
+      }).then(res => res.json()).then(res => {
+        console.log(res);
+      });
+    }
+    else {
+      this.setState({ isDialogOpen: true })
+
+    }
+  }
+
+  handleClickIcon() {
+    this.setState({ showPassword: !this.state.showPassword })
   }
 
   validatePassword(event) {
-    var lowerCaseLetters = /[a-z]/g;
-    if (event.target.value.match(lowerCaseLetters)) {
-      document.getElementById("letter").classList.remove("invalid");
-      document.getElementById("letter").classList.add("valid");
-    } else {
-      document.getElementById('letter').classList.remove("valid");
-      document.getElementById('letter').classList.add("invalid");
+    const pattern = new RegExp(/\w{8,}/)
+    this.setState({ passwordHelperText: "block" })
+    if (event.target.value.match(pattern)) {
+      this.setState({ password: event.target.value, passwordColor: "green" })
     }
-
-    // Validate capital letters
-    var upperCaseLetters = /[A-Z]/g;
-    if (event.target.value.match(upperCaseLetters)) {
-      document.getElementById("capital").classList.remove("invalid");
-      document.getElementById("capital").classList.add("valid");
-    } else {
-      document.getElementById("capital").classList.remove("valid");
-      document.getElementById("capital").classList.add("invalid");
-    }
-
-    // Validate numbers
-    var numbers = /[0-9]/g;
-    if (event.target.value.match(numbers)) {
-      document.getElementById("number").classList.remove("invalid");
-      document.getElementById("number").classList.add("valid");
-    } else {
-      document.getElementById("number").classList.remove("valid");
-      document.getElementById("number").classList.add("invalid");
-    }
-
-    // Validate length
-    if (event.target.value.length >= 8) {
-      document.getElementById("length").classList.remove("invalid");
-      document.getElementById("length").classList.add("valid");
-    } else {
-      document.getElementById("length").classList.remove("valid");
-      document.getElementById("length").classList.add("invalid");
+    else {
+      this.setState({ password: event.target.value, passwordColor: "red" })
     }
   }
 
-  handleClickShowPasssword(){
-    this.setState({showPassword:!this.state.showPassword})
+  confirmPassword(event) {
+    this.setState({ confirmHelperText: "block" })
+    if (this.state.password == event.target.value) {
+      this.setState({ isPasswordMatch: true, confirmPasswordColor: "green" })
+    }
+    else {
+      this.setState({ isPasswordMatch: false, confirmPasswordColor: "red" })
+    }
   }
 
   render() {
     return (
       <div>
-       
-          <div>
+        <h1>Create New Account</h1>
+        <div>
           <FormControl className="formElements" >
             <InputLabel>Name</InputLabel>
-            <Input autoFocus="true" id="new-name" type="name"/>
+            <Input
+              autoFocus={true}
+              onChange={(event) => this.setState({ name: event.target.value })}
+            />
           </FormControl>
-          </div>
-          
-          <div>
+        </div>
+
+        <div>
           <FormControl className="formElements" >
             <InputLabel >Email</InputLabel>
-            <Input id="email" type="email"/>
+            <Input
+              type="email"
+              onChange={(event) => this.setState({ email: event.target.value })}
+            />
           </FormControl>
-          </div>
-          
-          <div>
+        </div>
+
+        <div>
           <FormControl className="formElements" >
             <InputLabel>Password</InputLabel>
             <Input
-              id="password" 
-              type={this.state.showPassword?'text':'password'}
+              id="password"
+              type={this.state.showPassword ? 'text' : 'password'}
+              onChange={this.validatePassword}
+              onFocus={this.validatePassword}
               endAdornment={
                 <InputAdornment position="end">
-                  <IconButton onClick={this.handleClickShowPasssword} >
-                    {this.state.showPassword?<VisibilityOff/>:<Visibility/>}
+                  <IconButton onClick={this.handleClickIcon} >
+                    {this.state.showPassword ? <VisibilityOff /> : <Visibility />}
                   </IconButton>
                 </InputAdornment>
 
-              }  
-              />
-              <FormHelperText>Password must have atleat 8 characters</FormHelperText>
+              }
+            />
+            <FormHelperText style={{ color: this.state.passwordColor, display: this.state.passwordHelperText }} >Password length must be between 8 to 15 characters characters</FormHelperText>
           </FormControl>
-          </div>
-          
-          <div>
+        </div>
+
+        <div>
           <FormControl className="formElements" >
             <InputLabel>Re-Enter Password</InputLabel>
-            <Input id="confirm" type="password"
-             />
+            <Input
+              id="confirm"
+              type="password"
+              onChange={this.confirmPassword}
+            />
+            <FormHelperText style={{ color: this.state.confirmPasswordColor, display: this.state.confirmHelperText }} >{this.state.isPasswordMatch ? 'Password Match' : 'Password Mismatch'}</FormHelperText>
           </FormControl>
-          </div>
-            
-              <div id="message" style={{ display: "none" }}>
-                <h3>Password must contain the following:</h3>
-                <p id="letter" className="invalid">
-                  A <b>lowercase</b> letter
-                </p>
-                <p id="capital" className="invalid">
-                  A <b>capital (uppercase)</b> letter
-                </p>
-                <p id="number" className="invalid">
-                  A <b>number</b>
-                </p>
-                <p id="length" className="invalid">
-                  Minimum <b>8 characters</b>
-                </p>
-              </div>
-
-              <div style={{ paddingTop: 25, textAlign: "center" }}>
-                <Button
-                  raised
-                  color="primary"
-                  type="submit"
-                  onClick={this.createUser}
-                >
-                  Create New Account
+        </div>
+        <div style={{ paddingTop: 25, textAlign: "center" }}>
+          <Button
+            raised
+            color="primary"
+            type="submit"
+            onClick={this.createUser}
+          >
+            Create New Account
                 </Button>
-              </div>
+        </div>
+          <GoogleButton
+          onClick={() => { console.log('button clicked') }}
+        />
       </div>
     );
   }

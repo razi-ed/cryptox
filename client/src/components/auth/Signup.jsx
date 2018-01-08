e05078c10271645f5c79cb871bc408e9c39e7868
Dattu Kumar <dattu.kumar@mountblue.io>
2018-01-08 13:55:48 +0530

    removed space in POST request

--- client/src/components/auth/Signup.jsx
+++ client/src/components/auth/Signup.jsx
@@ -39,7 +39,7 @@ export default class SignUp extends React.Component {
     console.log(this.state.name, this.state.email, this.state.password);
     if (this.state.name && this.state.email && this.state.password) {
       fetch(' /auth/register', {
-        method: ' POST',
+        method: 'POST',
         headers: {
           'Content-Type': 'application/json',
         },

d22e4880f324ebf99a7fb9c7d28d13d9d9df458c
Dattu Kumar <dattu.kumar@mountblue.io>
2018-01-08 12:43:00 +0530

    adhered to google standars by using eslint

--- client/src/components/auth/Signup.jsx
+++ client/src/components/auth/Signup.jsx
@@ -6,143 +6,174 @@ import Visibility from 'material-ui-icons/Visibility';
 import VisibilityOff from 'material-ui-icons/VisibilityOff';
 import IconButton from 'material-ui/IconButton';
 
+/**
+ * this class creates a component for signup
+ */
 export default class SignUp extends React.Component {
+  /**
+   * @param {props} props
+   */
   constructor(props) {
     super(props);
     this.state = {
-      name:"",
-      email:"",
-      password:"",
+      name: ' ',
+      email: ' ',
+      password: ' ',
       showPassword: false,
-      passwordColor:"red",
-      passwordHelperText:"none",
-      confirmPasswordColor:"red",
-      confirmHelperText:"none",
-      isPasswordMatch:false,
-      isPasswordSet:false,
+      passwordColor: ' red',
+      passwordHelperText: ' none',
+      confirmPasswordColor: ' red',
+      confirmHelperText: ' none',
+      isPasswordMatch: false,
+      isPasswordSet: false,
     };
     this.validatePassword=this.validatePassword.bind(this);
     this.confirmPassword=this.confirmPassword.bind(this);
     this.createUser=this.createUser.bind(this);
     this.handleClickIcon=this.handleClickIcon.bind(this);
   }
+  /**
+   * this function sends a post request to registerUser api to create a new user
+   */
   createUser() {
-    console.log(this.state.name,this.state.email,this.state.password)
-    if(this.state.name && this.state.email && this.state.password){
-      fetch("/auth/register", {
-        method: "POST",
-        headers:{
-          'Content-Type':'application/json'
+    console.log(this.state.name, this.state.email, this.state.password);
+    if (this.state.name && this.state.email && this.state.password) {
+      fetch(' /auth/register', {
+        method: ' POST',
+        headers: {
+          'Content-Type': 'application/json',
         },
         body: JSON.stringify({
-          name:this.state.name,
+          name: this.state.name,
           email: this.state.email,
-          password:this.state.password
-        })
-      }).then(res=>res.json()).then(res => {
+          password: this.state.password,
+        }),
+      }).then((res)=>res.json()).then((res) => {
         console.log(res);
       });
-    }
-    else{
-      this.setState({isDialogOpen:true})
-     
+    } else {
+      this.setState({isDialogOpen: true});
     }
   }
-
-  handleClickIcon(){
-    this.setState({showPassword:!this.state.showPassword})
+  /**
+   * this function is an event handler which is called when eye icon is clicked
+   */
+  handleClickIcon() {
+    this.setState({showPassword: !this.state.showPassword});
   }
 
-  handleMouseDownIcon(event){
+  /**
+   * this fuction is called to prevent default action when eye icon is clicked
+   * @param {event} event
+   */
+  handleMouseDownIcon(event) {
     event.preventDefault();
   }
-
-
-  validatePassword(event){
-    const pattern=new RegExp(/\w{8,}/)
-    this.setState({passwordHelperText:"block"})
-    if(event.target.value.match(pattern)){
-      this.setState({password:event.target.value,passwordColor:"green"})
-    }
-    else{
-      this.setState({password:event.target.value,passwordColor:"red"})
-    }
-  }
-
-  confirmPassword(event){
-    this.setState({confirmHelperText:"block"})
-    if(this.state.password==event.target.value){
-      this.setState({isPasswordMatch:true,confirmPasswordColor:"green"})
-    }
-    else{
-      this.setState({isPasswordMatch:false,confirmPasswordColor:"red"})
+  /**
+   * this function is called when there is change in value in password field
+   * @param {event} event
+   */
+  validatePassword(event) {
+    const pattern=new RegExp(/\w{8,}/);
+    this.setState({passwordHelperText: ' block'});
+    if (event.target.value.match(pattern)) {
+      this.setState({password: event.target.value, passwordColor: ' green'});
+    } else {
+      this.setState({password: event.target.value, passwordColor: ' red'});
     }
   }
-
+  /**
+   * this function is called where there is change in Re-Enter password field
+   * @param {event} event
+   */
+  confirmPassword(event) {
+    this.setState({confirmHelperText: ' block'});
+    if (this.state.password==event.target.value) {
+      this.setState({isPasswordMatch: true, confirmPasswordColor: ' green'});
+    } else {
+      this.setState({isPasswordMatch: false, confirmPasswordColor: ' red'});
+    }
+  }
+  /**
+   * this function is called when react compnent is called to render
+   * @return {component} component to be mounted
+   */
   render() {
     return (
       <div>
-          <h1 style={{textAlign:"center"}} >Create New Account</h1>
+          <h1 style={{textAlign: 'center'}} >Create New Account</h1>
           <div>
-          <FormControl className="formElements" >
+          <FormControl className='formElements'>
             <InputLabel>Name</InputLabel>
-            <Input 
-              autoFocus={true} 
-              onChange={(event)=>this.setState({name:event.target.value})}
+            <Input
+              autoFocus={true}
+              onChange={(event)=>this.setState({name: event.target.value})}
               />
           </FormControl>
           </div>
-          
           <div>
-          <FormControl className="formElements" >
+          <FormControl className='formElements'>
             <InputLabel >Email</InputLabel>
-            <Input 
-              type="email"
-              onChange={(event)=>this.setState({email:event.target.value})}
+            <Input
+              type='email'
+              onChange={(event)=>this.setState({email: event.target.value})}
             />
           </FormControl>
           </div>
-          
           <div>
-          <FormControl className="formElements" >
+          <FormControl className='formElements'>
             <InputLabel>Password</InputLabel>
             <Input
-              id="password" 
-              type={this.state.showPassword?'text':'password'}
+              type={this.state.showPassword?'text': 'password'}
               onChange={this.validatePassword}
               onFocus={this.validatePassword}
               endAdornment={
-                <InputAdornment position="end">
-                  <IconButton 
+                <InputAdornment position='end' >
+                  <IconButton
                     onClick={this.handleClickIcon}
                     onMouseDown={this.handleMouseDownIcon}
                   >
-                    {this.state.showPassword?<VisibilityOff/>:<Visibility/>}
+                    {this.state.showPassword?<VisibilityOff/>: <Visibility/>}
                   </IconButton>
                 </InputAdornment>
-
-              }  
+              }
               />
-              <FormHelperText style={{color:this.state.passwordColor,display:this.state.passwordHelperText}} >Password length must be atleast 8 characters</FormHelperText>
+              <FormHelperText
+                style={
+                  {
+                    color: this.state.passwordColor,
+                    display: this.state.passwordHelperText,
+                  }
+                }
+              >
+              Password length must be atleast 8 characters
+              </FormHelperText>
           </FormControl>
           </div>
-          
           <div>
-          <FormControl className="formElements" >
+          <FormControl className='formElements'>
             <InputLabel>Re-Enter Password</InputLabel>
-            <Input 
-              id="confirm" 
-              type="password"
+            <Input
+              type=' password'
               onChange={this.confirmPassword}
              />
-            <FormHelperText style={{color:this.state.confirmPasswordColor,display:this.state.confirmHelperText}} >{this.state.isPasswordMatch?'Password Match':'Password Mismatch'}</FormHelperText>
+            <FormHelperText
+              style={
+                {
+                  color: this.state.confirmPasswordColor,
+                  display: this.state.confirmHelperText,
+                }
+              }
+            >
+              {this.state.isPasswordMatch?'Password Match': 'Password Mismatch'}
+            </FormHelperText>
           </FormControl>
           </div>
-              <div style={{ paddingTop: 25, textAlign: "center" }}>
+              <div style={{paddingTop: 25, textAlign: ' center'}}>
                 <Button
                   raised
-                  color="primary"
-                  type="submit"
+                  color=' primary'
+                  type=' submit'
                   onClick={this.createUser}
                 >
                   Create New Account
--- server/server.js
+++ server/server.js
@@ -1,40 +1,37 @@
-const express = require('express')
-const Mongoose=  require('mongoose')
+const express = require('express');
+const Mongoose= require('mongoose');
 const bodyParser = require('body-parser');
-const path= require('path')
-const passport= require('passport')
+const path= require('path');
+const passport= require('passport');
 
-const auth= require('./routes/auth')
-const frontend= require('./routes/frontend')
+const auth= require('./routes/auth');
+const frontend= require('./routes/frontend');
 
-//key
-process.env.JWT_KEY="587b2cc7-5a21-447f-8aef-28c2857a5db6"
-const app = express()
-Mongoose.Promise=global.Promise
- 
-app.use(bodyParser.urlencoded({ extended: false }));
+//  key
+process.env.JWT_KEY='587b2cc7-5a21-447f-8aef-28c2857a5db6';
+const app = express();
+Mongoose.Promise=global.Promise;
+app.use(bodyParser.urlencoded({extended: false}));
 app.use(bodyParser.json());
-
-
-//pasport init
+//  pasport init
 app.use(passport.initialize());
-require('./config/passport')(passport)
-//app.use(routes)
+require('./config/passport')(passport);
+//  app.use(routes)
 app.use(express.static(path.join(__dirname, '../dist/')));
-app.use("/auth",auth)
-app.use(frontend)
+app.use('/auth', auth);
+app.use(frontend);
 
 Mongoose
-  .connect(process.env.DB_HOST ||'mongodb://localhost/cryptox',{
-    useMongoClient:true
-  })
+  .connect(process.env.DB_HOST ||'mongodb://localhost/cryptox', {
+    useMongoClient: true,
+  });
 
 Mongoose
   .connection
-  .once('once',()=>console.log('copnnected to db'))
-  .on('error',(e)=>console.log('error connectin to db',e))
+  .once('once', ()=>console.log('copnnected to db'))
+  .on('error', (e)=>console.log('error connectin to db', e));
 
 
 app.listen(process.env.port || 5000, function() {
-  console.log('now listening for requests in port 5000')
-})
+  console.log('now listening for requests in port 5000');
+});

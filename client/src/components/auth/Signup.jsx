72ce25ad39538a607795e168f0222000ba1008ba
Dattu Kumar <dattu.kumar@mountblue.io>
2018-01-08 13:24:59 +0530

    created signUp component

--- client/src/components/auth/Signup.jsx
+++ client/src/components/auth/Signup.jsx
@@ -5,162 +5,175 @@ import {FormHelperText, FormControl} from 'material-ui/Form';
 import Visibility from 'material-ui-icons/Visibility';
 import VisibilityOff from 'material-ui-icons/VisibilityOff';
 import IconButton from 'material-ui/IconButton';
+
 /**
- * A class to create a signup component
+ * this class creates a component for signup
  */
 export default class SignUp extends React.Component {
- /**
-  * @param {props} props
-  */
+  /**
+   * @param {props} props
+   */
   constructor(props) {
     super(props);
     this.state = {
-      name: '',
-      email: '',
-      password: '',
+      name: ' ',
+      email: ' ',
+      password: ' ',
       showPassword: false,
-      passwordColor: 'red',
-      passwordHelperText: 'none',
-      confirmPasswordColor: 'red',
-      confirmHelperText: 'none',
+      passwordColor: ' red',
+      passwordHelperText: ' none',
+      confirmPasswordColor: ' red',
+      confirmHelperText: ' none',
       isPasswordMatch: false,
       isPasswordSet: false,
     };
-    this.handleClickShowPasssword=this.handleClickShowPasssword.bind(this)
+    this.validatePassword=this.validatePassword.bind(this);
+    this.confirmPassword=this.confirmPassword.bind(this);
+    this.createUser=this.createUser.bind(this);
+    this.handleClickIcon=this.handleClickIcon.bind(this);
   }
- 
-  createUser(){
-    console.log(document.getElementById('name').value)
-    fetch('/auth/register',{
-      method: 'POST',
-      headers: {
-      'Content-Type': 'application/json',
-      },
-      body: JSON.stringify({
-        name: document.getElementById('name').value,
-      email: document.getElementById('email').value,
-      password: document.getElementById('password').value,
-      })
-    }).then(res=>res.json()).then((res)=>{
-      console.log(res);
-    });
+  /**
+   * this function sends a post request to registerUser api to create a new user
+   */
+  createUser() {
+    console.log(this.state.name, this.state.email, this.state.password);
+    if (this.state.name && this.state.email && this.state.password) {
+      fetch(' /auth/register', {
+        method: ' POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+          name: this.state.name,
+          email: this.state.email,
+          password: this.state.password,
+        }),
+      }).then((res)=>res.json()).then((res) => {
+        console.log(res);
+      });
+    } else {
+      this.setState({isDialogOpen: true});
+    }
+  }
+  /**
+   * this function is an event handler which is called when eye icon is clicked
+   */
+  handleClickIcon() {
+    this.setState({showPassword: !this.state.showPassword});
   }
 
-  handleMouseDownIcon(event){
+  /**
+   * this fuction is called to prevent default action when eye icon is clicked
+   * @param {event} event
+   */
+  handleMouseDownIcon(event) {
     event.preventDefault();
   }
-
-
-  validatePassword(event){
-    const pattern=new RegExp(/\w{8,}/)
-    this.setState({passwordHelperText: 'block'})
-    if(event.target.value.match(pattern)){
-      this.setState({password: event.target.value,passwordColor: 'green'})
-    }
-
-    // Validate capital letters
-    var upperCaseLetters = /[A-Z]/g;
-    if (event.target.value.match(upperCaseLetters)) {
-      document.getElementById('capital').classList.remove('invalid');
-      document.getElementById('capital').classList.add('valid');
+  /**
+   * this function is called when there is change in value in password field
+   * @param {event} event
+   */
+  validatePassword(event) {
+    const pattern=new RegExp(/\w{8,}/);
+    this.setState({passwordHelperText: ' block'});
+    if (event.target.value.match(pattern)) {
+      this.setState({password: event.target.value, passwordColor: ' green'});
     } else {
-      document.getElementById('capital').classList.remove('valid');
-      document.getElementById('capital').classList.add('invalid');
-    }
-
-    // Validate numbers
-    var numbers = /[0-9]/g;
-    if (event.target.value.match(numbers)) {
-      document.getElementById('number').classList.remove('invalid');
-      document.getElementById('number').classList.add('valid');
-    } else {
-      document.getElementById('number').classList.remove('valid');
-      document.getElementById('number').classList.add('invalid');
-    }
-
-    // Validate length
-    if (event.target.value.length >= 8) {
-      document.getElementById('length').classList.remove('invalid');
-      document.getElementById('length').classList.add('valid');
-    } else {
-      document.getElementById('length').classList.remove('valid');
-      document.getElementById('length').classList.add('invalid');
+      this.setState({password: event.target.value, passwordColor: ' red'});
     }
   }
-
-  handleClickShowPasssword(){
-    this.setState({showPassword: !this.state.showPassword})
+  /**
+   * this function is called where there is change in Re-Enter password field
+   * @param {event} event
+   */
+  confirmPassword(event) {
+    this.setState({confirmHelperText: ' block'});
+    if (this.state.password==event.target.value) {
+      this.setState({isPasswordMatch: true, confirmPasswordColor: ' green'});
+    } else {
+      this.setState({isPasswordMatch: false, confirmPasswordColor: ' red'});
+    }
   }
-
+  /**
+   * this function is called when react compnent is called to render
+   * @return {component} component to be mounted
+   */
   render() {
     return (
       <div>
           <h1 style={{textAlign: 'center'}} >Create New Account</h1>
           <div>
-          <FormControl className='formElements' >
+          <FormControl className='formElements'>
             <InputLabel>Name</InputLabel>
-            <Input autoFocus='true' id='new-name' type='name'/>
+            <Input
+              autoFocus={true}
+              onChange={(event)=>this.setState({name: event.target.value})}
+              />
           </FormControl>
           </div>
-          
           <div>
-          <FormControl className='formElements' >
+          <FormControl className='formElements'>
             <InputLabel >Email</InputLabel>
-            <Input id='email' type='email'/>
+            <Input
+              type='email'
+              onChange={(event)=>this.setState({email: event.target.value})}
+            />
           </FormControl>
           </div>
-          
           <div>
-          <FormControl className='formElements' >
+          <FormControl className='formElements'>
             <InputLabel>Password</InputLabel>
             <Input
-              id='password' 
               type={this.state.showPassword?'text': 'password'}
+              onChange={this.validatePassword}
+              onFocus={this.validatePassword}
               endAdornment={
-                <InputAdornment position='end'>
-                  <IconButton 
+                <InputAdornment position='end' >
+                  <IconButton
                     onClick={this.handleClickIcon}
                     onMouseDown={this.handleMouseDownIcon}
                   >
                     {this.state.showPassword?<VisibilityOff/>: <Visibility/>}
                   </IconButton>
                 </InputAdornment>
-
-              }  
+              }
               />
-              <FormHelperText style={{color: this.state.passwordColor,display: this.state.passwordHelperText}} >Password length must be atleast 8 characters</FormHelperText>
+              <FormHelperText
+                style={
+                  {
+                    color: this.state.passwordColor,
+                    display: this.state.passwordHelperText,
+                  }
+                }
+              >
+              Password length must be atleast 8 characters
+              </FormHelperText>
           </FormControl>
           </div>
-          
           <div>
-          <FormControl className='formElements' >
+          <FormControl className='formElements'>
             <InputLabel>Re-Enter Password</InputLabel>
-            <Input id='confirm' type='password'
+            <Input
+              type=' password'
+              onChange={this.confirmPassword}
              />
+            <FormHelperText
+              style={
+                {
+                  color: this.state.confirmPasswordColor,
+                  display: this.state.confirmHelperText,
+                }
+              }
+            >
+              {this.state.isPasswordMatch?'Password Match': 'Password Mismatch'}
+            </FormHelperText>
           </FormControl>
           </div>
-            
-              <div id='message' style={{ display:  'none' }}>
-                <h3>Password must contain the following: </h3>
-                <p id='letter' className='invalid'>
-                  A <b>lowercase</b> letter
-                </p>
-                <p id='capital' className='invalid'>
-                  A <b>capital (uppercase)</b> letter
-                </p>
-                <p id='number' className='invalid'>
-                  A <b>number</b>
-                </p>
-                <p id='length' className='invalid'>
-                  Minimum <b>8 characters</b>
-                </p>
-              </div>
-
-              <div style={{ paddingTop:  25, textAlign:  'center' }}>
+              <div style={{paddingTop: 25, textAlign: ' center'}}>
                 <Button
                   raised
-                  color='primary'
-                  type='submit'
+                  color=' primary'
+                  type=' submit'
                   onClick={this.createUser}
                 >
                   Create New Account
--- client/src/components/auth/TabForSignUp.jsx
+++ client/src/components/auth/TabForSignUp.jsx
@@ -1,17 +1,20 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { withStyles } from 'material-ui/styles';
 import AppBar from 'material-ui/AppBar';
-import Tabs, { Tab } from 'material-ui/Tabs';
+import Tabs, {Tab} from 'material-ui/Tabs';
 import Typography from 'material-ui/Typography';
 import LogIn from './Login';
-import SignUp from './Signup'
+import SignUp from './Signup';
 import Grid from 'material-ui/Grid';
 
-
+/**
+ * this fuction takes props as parameter and creates padding for tab
+ * @param {props} props
+ * @return {component} Typography component
+ */
 function TabContainer(props) {
   return (
-    <Typography component="div" style={{ padding: 8 * 3 }}>
+    <Typography component='div' style={{ padding: 8 * 3 }}>
       {props.children}
     </Typography>
   );
@@ -29,29 +32,49 @@ const styles = theme => ({
   },
 });
 
+/**
+ * class to create tab structure for signup and login
+ */
 export default class BasicTabs extends React.Component {
-  state  ={
-    value: 0
+  /**
+   * constructor to initialize object
+   */
+  constructor() {
+    super();
+    this.state={
+      value: 0,
+    };
+  }
+  /**
+   * @param {event} event
+   * @param {value} value
+   * this function sets state value to current tab that is being viewed
+   */
+  handleChange(event, value) {
+    this.setState({value});
   };
-
-  handleChange = (event, value) => {
-    this.setState({ value });
-  };
-
+  /**
+   * this function renders the tab component
+   * @return {component} tab component
+   */
   render() {
-    const { classes } = this.props;
-    const { value } = this.state;
+    const {classes}=this.props;
+    const {value} = this.state;
 
     return (
       <div className={this.props.root}>
       <Grid container>
       <Grid item sm={3} xs={2}/>
-      <Grid item sm={6} xs={2} style={{alignItems:"center"}}>
+      <Grid item sm={6} xs={2} style={{alignItems: 'center'}}>
       <div>
-        <AppBar className="tabs" position="static" style={{justifyContent:"space-around"}}>
+        <AppBar
+          className='tabs'
+          position='static'
+          style={{justifyContent: 'space-around'}}
+        >
           <Tabs value={value} onChange={this.handleChange} >
-            <Tab label="LogIn" />
-            <Tab label="SignUp" />
+            <Tab label='LogIn' />
+            <Tab label='SignUp' />
           </Tabs>
         </AppBar>
         {value === 0 && <TabContainer><LogIn/></TabContainer>}
@@ -61,7 +84,6 @@ export default class BasicTabs extends React.Component {
         </Grid>
         <Grid item sm={3} xs={2}/>
       </div>
-      
     );
   }
 }

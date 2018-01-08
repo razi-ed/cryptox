6ec45c8f3031ceecf49e04b4f57e7bd6ba8a6665
akshay upadhya <akshaysrinagar@gmail.com>
2018-01-09 00:41:28 +0530

    fixing eslint issues -webpack.config.js

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -1,7 +1,7 @@
 const path = require('path');
-const webpack = require('webpack')
+const webpack = require('webpack');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
-const ExtractTextPlugin = require('extract-text-webpack-plugin')
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
 
 // Constant with our paths
 const paths = {
@@ -13,40 +13,40 @@ const paths = {
 // Webpack configuration
 module.exports = {
   entry: [
-    "webpack-hot-middleware/client",
-    "react-hot-loader/patch",
-    path.join(paths.JS, "Main.jsx")
+    'webpack-hot-middleware/clien',
+    'react-hot-loader/patch',
+    path.join(paths.JS, 'Main.jsx'),
   ],
   output: {
     path: paths.DIST,
     publicPath: '/',
-    filename: "app.bundle.js"
+    filename: 'app.bundle.js',
   },
-  devtool: "source-map",
+  devtool: 'source-map',
   plugins: [
     new HtmlWebpackPlugin({
-      template: path.join(paths.SRC, "index.html")
+      template: path.join(paths.SRC, 'index.html'),
     }),
-    new ExtractTextPlugin("style.bundle.css"),
+    new ExtractTextPlugin('style.bundle.css'),
     new webpack.NoEmitOnErrorsPlugin(),
-    new webpack.HotModuleReplacementPlugin()
+    new webpack.HotModuleReplacementPlugin(),
   ],
   module: {
     rules: [{
         test: /\.(js|jsx)$/,
         exclude: /node_modules/,
         use: [{
-          loader: "babel-loader",
+          loader: 'babel-loader',
           options: {
-            sourceMap: true
-          }
-        }]
+            sourceMap: true,
+          },
+        }],
       },
       {
         test: /\.css$/,
         loader: ExtractTextPlugin.extract({
-          use: "css-loader"
-        })
+          use: 'css-loader',
+        }),
       },
       {
         test: /\.(png|jpg|gif)$/,
@@ -56,11 +56,11 @@ module.exports = {
       },
       {
         test: /\.svg$/,
-        loader: 'svg-inline-loader'
-      }
+        loader: 'svg-inline-loader',
+      },
     ],
   },
   resolve: {
-    extensions: [" ", ".js", ".jsx"]
-  }
+    extensions: [' ', '.js', '.jsx']
+  },
 };

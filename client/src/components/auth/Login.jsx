4eb4e5155da3222ea5f8d347cfd81b92ebc40ba1
Shweta Mishra <shweta.mishra@mountblue.io>
2018-01-09 17:47:47 +0530

    fix eslint errors

--- client/src/components/auth/Login.jsx
+++ client/src/components/auth/Login.jsx
@@ -1,4 +1,4 @@
-import React, {Component} from 'react';
+import {Component} from 'react';
 import Button from 'material-ui/Button';
 import Grid from 'material-ui/Grid/Grid';
 import {FormHelperText, FormControl} from 'material-ui/Form';
@@ -8,73 +8,93 @@ import VisibilityOff from 'material-ui-icons/VisibilityOff';
 import IconButton from 'material-ui/IconButton';
 import GoogleButton from 'react-google-button';
 import '../../css/style.css';
-import ForgotPassword from './ForgotPassword';
 
+/**
+ * @class
+ */
 export default class Login extends Component {
-  constructor(){
+  /**
+   * @constructor
+   */
+  constructor() {
     super();
     this.state = {
     email: '',
     password: '',
     showPassword: false,
     validationHelperTextVisible: 'none',
-    validationColor: 'red',  
+    validationColor: 'red',
   };
-  this.changeEmail = this.changeEmail.bind(this)
-  this.changePassword = this.changePassword.bind(this)
-  this.loginUser = this.loginUser.bind(this)
-  this.handleClickIcon = this.handleClickIcon.bind(this)
-  this.handleMouseDownIcon = this.handleMouseDownIcon.bind(this)
+  this.changeEmail = this.changeEmail.bind(this);
+  this.changePassword = this.changePassword.bind(this);
+  this.loginUser = this.loginUser.bind(this);
+  this.handleClickIcon = this.handleClickIcon.bind(this);
+  this.handleMouseDownIcon = this.handleMouseDownIcon.bind(this);
 }
-
-  loginUser(){
-    
-    fetch("/auth/login",{
-      method:"POST",
-      headers:{
-        'Content-Type':'application/json',
+/**
+ * @function
+ */
+  loginUser() {
+    fetch('/auth/login', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
       },
-      body:JSON.stringify({
-        email:this.state.email,
-        password: this.state.password
-      })
-    }).then(res=>res.text()).then((res)=>{
+      body: JSON.stringify({
+        email: this.state.email,
+        password: this.state.password,
+      }),
+    }).then((res)=>res.text()).then((res)=>{
       if (!res.success) {
-        console.log(res.message)
-        this.setState({ validationHelperTextVisible: ' block' });
+        console.log(res.message);
+        this.setState({validationHelperTextVisible: ' block'});
       }
 
       console.log(res);
-    })
+    });
   }
-  
+  /**
+   * @param {event} event
+   */
   changeEmail(event) {
     this.setState({
-      email: event.target.value
+      email: event.target.value,
     });
     console.log(this.state.email);
   }
-
+/**
+ *
+ * @param {event} event
+ */
   changePassword(event) {
     this.setState({
-      password: event.target.value
+      password: event.target.value,
     });
-    console.log(this.state.password)
+    console.log(this.state.password);
   }
-
+/**
+ *@function
+ */
   handleClickIcon() {
-    this.setState({ showPassword: !this.state.showPassword });
+    this.setState({showPassword: !this.state.showPassword});
   }
-
+/**
+ *
+ * @param {event} event
+ */
   handleMouseDownIcon(event) {
     event.preventDefault();
   }
+/**
+ * @function
+ * @return {loginhtml}
+ * */
   render() {
     return (
-      <div id='form'>       
+      <div id='form'>
         <Grid item lg={12}>
-            <h1 style={{textAlign:"center"}}>Log Into Your Account</h1>
-          
+            <h1 style={{textAlign: 'center'}}>Log Into Your Account</h1>
+
           <FormControl className='formElements' >
             <InputLabel >Email</InputLabel>
             <Input
@@ -94,7 +114,8 @@ export default class Login extends Component {
                     onClick={this.handleClickIcon}
                     onMouseDown={this.handleMouseDownIcon}
                   >
-                    {this.state.showPassword ? <VisibilityOff /> : <Visibility />}
+                    {this.state.showPassword ? <VisibilityOff />
+                     : <Visibility />}
                   </IconButton>
                 </InputAdornment>
               }
@@ -112,7 +133,10 @@ export default class Login extends Component {
             </FormHelperText>
         </Grid>
             <div id='button_line'>
-              <Button type="submit" raised color="primary"  className="button" onClick={this.loginUser}>
+              <Button type="submit"
+              raised color="primary"
+              className="button"
+              onClick={this.loginUser}>
                 Log In
               </Button>
             </div>
@@ -123,10 +147,12 @@ export default class Login extends Component {
           <div id="signG">
             <GoogleButton
               type="dark"
-              onClick={() => { console.log('this will not run on click since it is disabled') }}
+              onClick={() => {
+              console.log('this will not run on click since it is disabled');
+              }}
             />
           </div>
-          
+
       </div>
     );
   }

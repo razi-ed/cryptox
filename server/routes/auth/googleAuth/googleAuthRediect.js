f3b55644940b6c9f41d1d1ad4dfe364da87fdb12
akshay upadhya <akshay.upadhya@mountblue.io>
2018-01-09 13:01:43 +0530

    removing all console logs

--- server/routes/auth/googleAuth/googleAuthRediect.js
+++ server/routes/auth/googleAuth/googleAuthRediect.js
@@ -2,7 +2,6 @@ const express= require('express');
 const Router = new express.Router();
 const passport = require('passport');
 Router.use(passport.authenticate('google'), (req, res) => {
-  console.log(req.query);
   res.redirect('/dashboard');
 });
 

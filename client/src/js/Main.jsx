18048cf79e27465e91f2768436938cf92850dcba
akshay upadhya <akshay.upadhya@mountblue.io>
2018-01-10 11:30:42 +0530

    updating unknown merge conflicts

--- client/src/js/Main.jsx
+++ client/src/js/Main.jsx
@@ -1,24 +1,8 @@
-<<<<<<< HEAD
-import React from 'react';
-import {render} from 'react-dom';
-import App from '../components/App';
-||||||| merged common ancestors
-import React, { Component } from 'react';
-import { render } from 'react-dom';
-import '../css/style.css'
-import App from '../components/App'
-=======
 import React from 'react';
 import {render} from 'react-dom';
 import '../css/style.css';
 import App from '../components/App';
->>>>>>> authentication
 
-<<<<<<< HEAD
-render( <App />, document.getElementById('app1'));
-||||||| merged common ancestors
-render( <App /> , document.getElementById('app1'));
-=======
 
 if (module.hot) {
     module.hot.accept(App, ()=>{
@@ -29,4 +13,3 @@ if (module.hot) {
 };
 
 render(<App />, document.getElementById('app'));
->>>>>>> authentication

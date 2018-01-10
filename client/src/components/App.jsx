dec78c92e39a8b1bab7b88877af6c7c0f2d361c5
akshay upadhya <akshay.upadhya@mountblue.io>
2018-01-10 11:19:48 +0530

    adding stashed changes

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -2,18 +2,7 @@ import React from 'react';
 import {BrowserRouter,Route} from 'react-router-dom';
 import LogIn from './auth/TabForSignUp';
 import Header from './Header';
-<<<<<<< Updated upstream
-import Home from '../components/home'
 
-||||||| merged common ancestors
-import Home from '../components/Home';
-import Footer from '../components/Footer';
-import ForgotPassword from '../components/auth/ForgotPassword';
-
-/**
- * this class loads all the required components for this project
- */
-=======
 import Home from '../components/Home';
 import Footer from '../components/Footer';
 import ForgotPassword from '../components/auth/ForgotPassword';
@@ -21,7 +10,7 @@ import '../css/style.css';
 /**
  * this class loads all the required components for this project
  */
->>>>>>> Stashed changes
+
 export default class App extends React.Component {
   render() {
     return (

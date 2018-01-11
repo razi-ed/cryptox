4dfc6e0706afdb7c3145bc599bde319fe388c080
Dattu Kumar <dattu.kumar@mountblue.io>
2018-01-11 11:54:12 +0530

    removed alert and used form required field

--- client/src/components/auth/Signup.jsx
+++ client/src/components/auth/Signup.jsx
@@ -7,12 +7,12 @@ import VisibilityOff from 'material-ui-icons/VisibilityOff';
 import IconButton from 'material-ui/IconButton';
 import Google from './Google';
 /**
- * this class creates a component for signup
- */
+* this class creates a component for signup
+*/
 export default class SignUp extends React.Component {
   /**
-   * @param {props} props
-   */
+  * @param {props} props
+  */
   constructor(props) {
     super(props);
     this.state = {
@@ -36,8 +36,8 @@ export default class SignUp extends React.Component {
     this.handleClickIcon=this.handleClickIcon.bind(this);
   }
   /**
-   * this function sends a post request to registerUser api to create a new user
-   */
+  * this function sends a post request to registerUser api to create a new user
+  */
   createUser() {
     if (this.state.name && this.state.email && this.state.password) {
       fetch(' /auth/register', {
@@ -53,28 +53,26 @@ export default class SignUp extends React.Component {
       }).then((res)=>res.json()).then((res) => {
         console.log(res);
       });
-    } else {
-        alert('Fill up all the fields ');
     }
   }
   /**
-   * this function is an event handler which is called when eye icon is clicked
-   */
+  * this function is an event handler which is called when eye icon is clicked
+  */
   handleClickIcon() {
     this.setState({showPassword: !this.state.showPassword});
   }
 
   /**
-   * this fuction is called to prevent default action when eye icon is clicked
-   * @param {event} event
-   */
+  * this fuction is called to prevent default action when eye icon is clicked
+  * @param {event} event
+  */
   handleMouseDownIcon(event) {
     event.preventDefault();
   }
   /**
-   * this function is called when there is change in value in password field
-   * @param {event} event
-   */
+  * this function is called when there is change in value in password field
+  * @param {event} event
+  */
   validatePassword(event) {
     this.setState({isPasswordHelperTextVisible: 'block'});
     const lowerCase=/([a-z])/g;
@@ -106,115 +104,140 @@ export default class SignUp extends React.Component {
               passwordHelperText: 'must contain minimum of 8 characters',
               passwordColor: 'red',
             }):
-          this.setState(
-            {
-              password: event.target.value,
-              passwordHelperText: 'Valid password',
-              passwordColor: 'green',
-            });
-  }
-  /**
-   * this function is called where there is change in Re-Enter password field
-   * @param {event} event
-   */
-  confirmPassword(event) {
-    this.setState({confirmHelperText: ' block'});
-    if (this.state.password==event.target.value) {
-      this.setState({isPasswordMatch: true, confirmPasswordColor: ' green'});
-    } else {
-      this.setState({isPasswordMatch: false, confirmPasswordColor: ' red'});
-    }
-  }
-  /**
-   * this function is called when react compnent is called to render
-   * @return {component}
-   */
-  render() {
-    return (
-      <div id='signup-frame'>
-          <h1 style={{textAlign: 'center'}} >Create New Account</h1>
-          <div>
-          <FormControl className='form-elements'>
-            <InputLabel>Name</InputLabel>
-            <Input
-              autoFocus={true}
-              onChange={(event)=>this.setState({name: event.target.value})}
-              />
-          </FormControl>
-          </div>
-          <div>
-          <FormControl className='form-elements'>
-            <InputLabel >Email</InputLabel>
-            <Input
-              type='email'
-              onChange={(event)=>this.setState({email: event.target.value})}
-            />
-          </FormControl>
-          </div>
-          <div>
-          <FormControl className='form-elements'>
-            <InputLabel>Password</InputLabel>
-            <Input
-              type={this.state.showPassword?'text': 'password'}
-              onChange={this.validatePassword}
-              onFocus={this.validatePassword}
-              endAdornment={
-                <InputAdornment position='end' >
-                  <IconButton
-                    onClick={this.handleClickIcon}
-                    onMouseDown={this.handleMouseDownIcon}
-                  >
-                    {this.state.showPassword?<VisibilityOff/>: <Visibility/>}
-                  </IconButton>
-                </InputAdornment>
+            this.setState(
+              {
+                password: event.target.value,
+                passwordHelperText: 'Valid password',
+                passwordColor: 'green',
+              });
+            }
+            /**
+            * this function is called when there is
+              change in Re-Enter password field
+            * @param {event} event
+            */
+            confirmPassword(event) {
+              this.setState({confirmHelperText: ' block'});
+              if (this.state.password==event.target.value) {
+                this.setState({
+                                isPasswordMatch: true,
+                                confirmPasswordColor: 'green',
+                              });
+              } else {
+                this.setState({
+                                isPasswordMatch: false,
+                                confirmPasswordColor: 'red',
+                              });
               }
-              />
-              <FormHelperText
+            }
+            /**
+            * this function is called when react compnent is called to render
+            * @return {component}
+            */
+            render() {
+              return (
+                <div id='signup-frame'>
+                <h1 style={{textAlign: 'center'}} >Create New Account</h1>
+                <form>
+                <div>
+
+                <FormControl
+                className='form-elements'
+                required={true}
+                >
+                <InputLabel>Name</InputLabel>
+                <Input
+                autoFocus={true}
+                onChange={(event)=>this.setState({name: event.target.value})}
+                />
+                </FormControl>
+                </div>
+                <div>
+                <FormControl
+                className='form-elements'
+                required={true}
+                >
+                <InputLabel >Email</InputLabel>
+                <Input
+                type='email'
+                onChange={(event)=>this.setState({email: event.target.value})}
+                />
+                </FormControl>
+                </div>
+                <div>
+                <FormControl
+                className='form-elements'
+                required={true}
+                >
+                <InputLabel>Password</InputLabel>
+                <Input
+                type={this.state.showPassword?'text': 'password'}
+                onChange={this.validatePassword}
+                onFocus={this.validatePassword}
+                endAdornment={
+                  <InputAdornment position='end' >
+                  <IconButton
+                  onClick={this.handleClickIcon}
+                  onMouseDown={this.handleMouseDownIcon}
+                  >
+                  {this.state.showPassword?<VisibilityOff/>: <Visibility/>}
+                  </IconButton>
+                  </InputAdornment>
+                }
+                />
+                <FormHelperText
                 style={
                   {
                     color: this.state.passwordColor,
                     display: this.state.isPasswordHelperTextVisible,
                   }
                 }
-              >
-             {this.state.passwordHelperText}
-              </FormHelperText>
-          </FormControl>
-          </div>
-          <div>
-          <FormControl className='form-elements'>
-            <InputLabel>Re-Enter Password</InputLabel>
-            <Input
-              type='password'
-              onChange={this.confirmPassword}
-             />
-            <FormHelperText
-              style={
-                {
-                  color: this.state.confirmPasswordColor,
-                  display: this.state.confirmHelperText,
-                }
-              }
-            >
-              {this.state.isPasswordMatch?'Password Match': 'Password Mismatch'}
-            </FormHelperText>
-          </FormControl>
-          </div>
-              <div style={{paddingTop: 25, textAlign: ' center'}}>
-                <Button
-                  raised
-                  color='primary'
-                  type='submit'
-                  onClick={this.createUser}
                 >
-                  Create New Account
+                {this.state.passwordHelperText}
+                </FormHelperText>
+                </FormControl>
+                </div>
+                <div>
+                <FormControl
+                className='form-elements'
+                required={true}
+                >
+                <InputLabel>Re-Enter Password</InputLabel>
+                <Input
+                type='password'
+                onChange={this.confirmPassword}
+                />
+                <FormHelperText
+                style={
+                  {
+                    color: this.state.confirmPasswordColor,
+                    display: this.state.confirmHelperText,
+                  }
+                }
+                >
+                {this.state.isPasswordMatch?
+                  'Password Match': 'Password Mismatch'}
+                </FormHelperText>
+                </FormControl>
+
+                </div>
+                <div style={{paddingTop: 25, textAlign: ' center'}}>
+                <Button
+                raised
+                color='primary'
+                type='submit'
+                onClick={this.createUser}
+                >
+                Create New Account
                 </Button>
                 <hr className="hr-text" data-content="Or"></hr>
-          <div id="google-signin">
-                  <Google/>
+                <div id="google-signin">
+                <Google/>
                 </div>
-              </div>
-      </div>
-    );
-  }
-}
+                </div>
+                </form>
+                </div>
+              );
+            }
+          }
+
--- client/webpack.config.js
+++ client/webpack.config.js
@@ -61,6 +61,6 @@ module.exports = {
     ],
   },
   resolve: {
-    extensions: [' ', '.js', '.jsx']
+    extensions: [' ', '.js', '.jsx'],
   },
 };

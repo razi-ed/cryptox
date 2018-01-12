b96586ce042807da159d39f0d818048455d1e6cd
akshay upadhya <akshaysrinagar@gmail.com>
2018-01-12 19:38:51 +0530

    adding babel-elsint

--- client/src/components/exchange/List.jsx
+++ client/src/components/exchange/List.jsx
@@ -13,52 +13,59 @@ export default class List extends React.Component {
     super(props);
     this.state={
       crypto: ['BTC', 'ETH', 'XRP', 'LTC', 'DASH'],
-      real: [ 'USD', 'EUR', 'JPY', 'GBP'],
-      BTC: 0, ETH: 0, XRP: 0, LTC: 0, DASH: 0, USD:0, EUR:0, JPY:0, GBP:0
+      real: ['USD', 'EUR', 'JPY', 'GBP'],
+      BTC: 0, ETH: 0, XRP: 0, LTC: 0, DASH: 0, USD: 0, EUR: 0, JPY: 0, GBP: 0,
     };
   }
   getCrypto = async () =>{
     console.log('get crpto');
-    try{
+    try {
       const response = await fetch('https://api.coinmarketcap.com/v1/ticker/?convert=INR&limit=15');
       const currencies = await response.json();
       this.state.crypto.forEach(Rcurrency=>{
         currencies
           .filter(currency=>currency.symbol==Rcurrency)
-          .forEach(crypto=>this.setState({[crypto.symbol]:Number(crypto.price_inr)}))
-      })
-    } catch(e){
-      console.log('error',e);
-    } finally{
-      console.log('done')
+          .forEach(crypto=>
+            this.setState({[crypto.symbol]: Number(crypto.price_inr)}));
+      });
+    } catch (e) {
+      console.log('error', e);
+    } finally {
+      console.log('done');
     }
   }
   getReal = async () =>{
     console.log('get real');
-    try{
+    try {
       const response = await fetch('https://api.fixer.io/latest?base=INR');
       const currencies = await response.json();
       this.state.real.forEach(Rcurrency=>{
-        this.setState({ [Rcurrency]: currencies.rates[Rcurrency]})
-      })
-    } catch(e){
-      console.log('error',e);
-    } finally{
-      console.log('done')
+        this.setState({[Rcurrency]: currencies.rates[Rcurrency]});
+      });
+    } catch (e) {
+      console.log('error', e);
+    } finally {
+      console.log('done');
     }
   }
-  componentDidMount () {
-     setInterval(()=>{
+  componentDidMount() {
+     setInterval(()=> {
       this.getCrypto();
       this.getReal();
-    },15000);
+    }, 15000);
   }
   render() {
     return (
-      <Grid className="list" item={true}  xs={10} md={5} alignItems='center'>
+      <Grid className="list" item={true} xs={10} md={5} alignItems='center'>
       <Paper className='List' >
-          {this.state.crypto.map(coin => <Currency name={coin} type='digital' price={this.state[coin] } />)}
-          {this.state.real.map(coin => <Currency name={coin}type='real' price={1 / this.state[coin]}/>)}
+          {this.state.crypto.map(coin =>
+             <Currency name={coin}
+                       type='digital'
+                       price={this.state[coin] } />)}
+          {this.state.real.map(coin =>
+             <Currency name={coin}
+                       type='real'
+                       price={1 / this.state[coin]} />)}
       </Paper>
       </Grid>
     );

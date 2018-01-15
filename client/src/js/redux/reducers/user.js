c136ab96502ae687a13294a1332111f919b86392
Razi <razi.mathilakath@gmail.com>
2018-01-15 20:31:50 +0530

    User reducer template

--- client/src/js/redux/reducers/user.js
+++ client/src/js/redux/reducers/user.js
@@ -1,6 +1,21 @@
-export default
-const userFunction = () => {
-  return {
-    IsAuthenticated: true;
+/**
+ * A template reducer that deals with user related states
+ * @param {Object} state
+ * @param {Object} action
+ * @return {Object} Updated state
+ */
+export default function user(state, action) {
+  switch (action.type) {
+    case 'LOG_IN':
+      {
+        state.email = action.email;
+        state.IsAuthenticated = true;
+        break;
+      }
+    case 'LOG_OUT':
+      {
+        state.email = 'Guest';
+        state.IsAuthenticated = false;
+        break;
+      }
   }
-}

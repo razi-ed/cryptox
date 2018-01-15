2037a974786aabd17feb759ffe02518fb81a127f
Dattu Kumar <dattu.kumar@mountblue.io>
2018-01-15 17:40:41 +0530

    added routes for getting user statement

---
+++ server/routes/accountStatements.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const router = new express.Router();
+const passport = require('passport');
+const orders = require('../models/ordersSchema');
+
+module.exports= router.post('/statement',
+            passport.authenticate('jwt', {session: false}),
+            (req, res)=>{
+              const email = req.user.email;
+            orders.find({email}).then((result)=>res.send(result));
+            });
--- server/server.js
+++ server/server.js
@@ -8,6 +8,7 @@ const auth = require('./routes/auth');
 const orders= require('./routes/order');
 const webpackDevMiddleware = require('webpack-dev-middleware');
 const webpackHotMiddleware = require('webpack-hot-middleware');
+const statement = require('./routes/accountStatements');
 
 const config = require('../client/webpack.config.js');
 const passportConfig = require('./config/passport');
@@ -61,6 +62,7 @@ passportConfig(passport);
 app.use(express.static(path.join(__dirname, '../dist/')));
 app.use('/auth', auth);
 app.use('/orders', orders);
+app.use('/user', statement);
 app.use(frontend);
 
 Mongoose

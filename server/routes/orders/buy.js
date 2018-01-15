8108bd40fd0d7eb9b72368c28c990a356b20fb5b
Dattu Kumar <dattu.kumar@mountblue.io>
2018-01-15 17:04:25 +0530

    created rest end points  for buy and sell

--- server/routes/orders/buy.js
+++ server/routes/orders/buy.js
@@ -1,21 +1,35 @@
 const Orders = require('../../models/ordersSchema');
+const traderAccount = require('../../models/traderAccountsSchema');
 const buying = (req, res)=>{
   const email=req.user.email;
   const units=req.body.units;
   const currency=req.body.type;
+  const updateTrader = {};
+  updateTrader['currencies.'+req.body.tradeFor]=(1/req.body.currentPrice)*units;
+  updateTrader['currencies.'+currency]= -units;
+  console.log(updateTrader);
+  const updateAdmin = {};
+  updateAdmin['currencies.'+req.body.tradeFor]=-(1/req.body.currentPrice)*units;
+  updateAdmin['currencies.'+currency] = units;
   const Order=new Orders({
     type: 'buyer',
     email,
     commodity: {
-                offer: {
-                  [currency]: units,
-                },
-                tradeFor: {
-                  [req.body.tradeFor]: 0.000071*units,
-              },
-              },
+      offer: {
+        [currency]: units,
+      },
+      tradeFor: {
+        [req.body.tradeFor]: (1/req.body.currentPrice)*units,
+      },
+    },
   });
   Order.save();
+  traderAccount.update(
+    {email},
+    {$inc: updateTrader}).then((result)=>console.log(result));
+  traderAccount.update(
+    {email: 'admin@mountblue.io'},
+    {$inc: updateAdmin}).then((result)=>console.log(result));
   res.send('Transaction is Successful');
-};
-module.exports = buying;
+  };
+  module.exports = buying;
--- server/routes/orders/sell.js
+++ server/routes/orders/sell.js
@@ -1,21 +1,36 @@
 const Orders = require('../../models/ordersSchema');
+const traderAccounts= require('../../models/traderAccountsSchema');
 const selling = (req, res)=>{
   const email=req.user.email;
   const units=req.body.units;
   const currency=req.body.type;
+  const updateTrader = {};
+  updateTrader['currencies.'+currency] = -units;
+  updateTrader['currencies.'+req.body.tradeFor]=1/req.body.currentPrice*units;
+  const updateAdmin = {};
+  updateAdmin['currencies.'+currency] = +units;
+  updateAdmin['currencies.'+req.body.tradeFor]=-(1/req.body.currentPrice)*units;
   const Order=new Orders({
     type: 'seller',
     email,
     commodity: {
-                offer: {
-                  [currency]: units,
-                },
-                tradeFor: {
-                  [req.body.tradeFor]: (14000*units),
-              },
-          },
-    });
+      offer: {
+        [currency]: units,
+      },
+      tradeFor: {
+        [req.body.tradeFor]: (1/req.body.cuurentPrice)*units,
+      },
+    },
+  });
   Order.save();
+  traderAccounts.update(
+    {email},
+    {$inc: updateTrader}
+  );
+  traderAccounts.update(
+    {email: 'admin@mountblue.io'},
+    {$inc: updateAdmin}
+  );
   res.send('Transaction is Successful');
-};
-module.exports = selling;
+  };
+    module.exports = selling;

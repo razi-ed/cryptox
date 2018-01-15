44136231146b48be1d416518de9516553899fc1d
Razi <razi.mathilakath@gmail.com>
2018-01-15 20:39:20 +0530

    Immutable User Reducer [Template]

--- client/src/js/redux/reducers/user.js
+++ client/src/js/redux/reducers/user.js
@@ -8,14 +8,13 @@ export default function user(state, action) {
   switch (action.type) {
     case 'LOG_IN':
       {
-        state.email = action.email;
-        state.IsAuthenticated = true;
+        state = {...state, email: action.payload.email, IsAuthenticated: true };
         break;
       }
     case 'LOG_OUT':
       {
-        state.email = 'Guest';
-        state.IsAuthenticated = false;
+        state = {...state, email: 'Guest', IsAuthenticated: false };
         break;
       }
+      return state
   }

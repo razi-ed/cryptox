85257577b746a3599830b1ac9022aa212160b127
Neel Parmar <neel.parmar@mountblue.io>
2018-01-16 11:23:00 +0530

    Redirect using withRouter method

--- client/src/components/dashboard/UserProfile.jsx
+++ client/src/components/dashboard/UserProfile.jsx
@@ -13,31 +13,10 @@ export default class UserProfile extends React.Component {
    */
   constructor() {
     super();
-    this.state = {
-      email: '',
-      name: '',
-    };
   }
   /**
    * @method
    */
-  componentDidMount() {
-    fetch('/profile', {
-      method: 'GET',
-      headers: {
-        'Authorization': localStorage.getItem('token'),
-        'Content-Type': 'application/x-www-form-urlencoded',
-      },
-    }).then((res) => {
-      return res.json();
-    }).then((res) => {
-      console.log(res);
-      this.setState({
-        email: res.email,
-        name: res.name,
-      });
-    }).catch();
-  }
   /**
    * @method
    * @return {userDetailsHTML}
@@ -47,8 +26,8 @@ export default class UserProfile extends React.Component {
       <div className='user-profile'>
         <Grid spacing={0} container justify='center'>
           <Paper>
-            {this.state.name}
-            {this.state.email}
+            {this.props.name}
+            {this.props.email}
           </Paper>
         </Grid>
       </div>
--- client/src/components/dashboard/dashboard.jsx
+++ client/src/components/dashboard/dashboard.jsx
@@ -2,19 +2,59 @@ import React from 'react';
 import Grid from 'material-ui/Grid';
 import Currency from './Currency';
 import UserProfile from './UserProfile';
+// import history from '../history';
+import {withRouter} from 'react-router-dom';
 /**
  * this class creates Dashboard component
  */
-export default class Dashboard extends React.Component {
-   /**
+class Dashboard extends React.Component {
+  /**
+   * @constructor
+   */
+  constructor() {
+    super();
+    this.state = {
+      email: '',
+      name: '',
+    };
+  }
+  /**
+   * @method
+   */
+  componentWillMount() {
+    if (localStorage.getItem('token')) {
+      fetch('/profile', {
+        method: 'GET',
+        headers: {
+          'Authorization': localStorage.getItem('token'),
+          'Content-Type': 'application/x-www-form-urlencoded',
+        },
+      }).then((res) => {
+        return res.json();
+      }).then((res) => {
+        console.log(res);
+        this.setState({
+          email: res.email,
+          name: res.name,
+        });
+      }).catch((error) => {
+        this.props.history.push('/login');
+      });
+    } else {
+      this.props.history.push('/login');
+    }
+  }
+  /**
    * this renders the component
    * @return {component}
    */
   render() {
+    console.log(this.state.name, this.state.email);
+
     return (
       <div className='currency-container'>
       <Grid className='user-profile-container' container>
-      <UserProfile />
+      <UserProfile name={this.state.name} email={this.state.email}/>
       </Grid>
       {/* <Grid className='currency-panel' container spacing={0}>
       <Currency currency = 'bitcoin'/>
@@ -27,3 +67,6 @@ export default class Dashboard extends React.Component {
     );
   };
 };
+
+export default withRouter(Dashboard);
+;

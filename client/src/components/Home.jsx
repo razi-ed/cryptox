1a32376f618c748c7c62afbdd14f359b1c5ffe22
Razi <razi.mathilakath@gmail.com>
2018-01-17 11:53:43 +0530

    Add Hero image.

--- client/src/components/Home.jsx
+++ client/src/components/Home.jsx
@@ -10,14 +10,14 @@ import {withTheme, createMuiTheme, MuiThemeProvider} from 'material-ui/styles';
 import {red, lightBlue} from 'material-ui/colors';
 import Button from 'material-ui/Button';
 import {Link} from 'react-router-dom';
-
-
+import Card, {CardActions, CardContent, CardMedia} from 'material-ui/Card';
+import Avatar from 'material-ui/Avatar';
+import HeroImage from '../../utils/landing_page.png'
 const theme = createMuiTheme({
   palette: {
     primary: lightBlue,
     secondary: red,
     accent: red,
-    contrast: '#FFFFFF',
   },
   status: {
     danger: 'orange',
@@ -29,13 +29,51 @@ const styles = (theme) => ({
   root: {
     flexGrow: 1,
   },
-  paper: {
-  },
+  paperColorNSize: {
+    backgroundColor: theme.palette.primary[500],
+    height: '90vh',
+    },
   button: {
     margin: theme.spacing.unit,
   },
   landingPage: {
+    backgroundColor: 'rgba(255,255,255,0.87)',
+    paddingBottom: 10,
+    paddingTop: 10,
+  },
+  landingPagePaper: {
     backgroundColor: theme.palette.primary[500],
+  },
+  paper: theme.mixins.gutters({
+  
+  }),
+  featuresPage: {
+    paddingTop: 40,
+    backgroundColor: theme.palette.text.secondary,
+  },
+  card: {
+    minWidth: 275,
+  },
+  media: {
+    height: 200,
+  },
+  bullet: {
+    display: 'inline-block',
+    margin: '0 2px',
+    transform: 'scale(0.8)',
+  },
+  title: {
+    marginBottom: 16,
+    fontSize: 14,
+    color: theme.palette.text.secondary,
+  },
+  pos: {
+    marginBottom: 12,
+    color: theme.palette.text.secondary,
+  },
+  heroImage: {
+    height: theme.spacing.unit *40,
+    width: theme.spacing.unit *50,
   },
 });
 /**
@@ -46,32 +84,52 @@ class Home extends React.Component {
    * this functions renders home component
    * @return {component} Home component
    */
+  state = {
+    direction: 'row',
+    justify: 'center',
+    alignItems: 'center',
+    elevationValue: 0
+  };
   render() {
     const {classes} = this.props;
+    const bull = <span className={classes.bullet}>•</span>;
 
     return (
       <div className = { classes.root }>
-        <Header />
-        <Grid container spacing={0} className = {classes.landingPage}>
-
-          <Grid item xs={12} sm={6} md={6}>
-            <Paper className={classes.paper}>
-              <Typography type="display3">
-                Control Your Future Wealth
-      </Typography>
-              <Typography type="subheading">
-                secure, manage and exchange cryptocurrencies
-      </Typography>
-             <Button raised color="accent"
-             className={classes.button}
-             component={Link} to='/login'>
-        Start Trading
+        <Header elevationValue={this.state.elevationValue}/>
+        <div>
+          <Paper className={[classes.paper, classes.paperColorNSize]} elevation={10}>
+          <Grid
+            container
+            spacing={12}
+            alignItems='center'
+            direction='column'
+            justify='space-around'
+          >
+          <Grid item>
+          <img className={classes.heroImage} src={HeroImage}/>            
+          </Grid>            
+          <Grid item>            
+          <Typography type="display3">
+            Control Your Future Wealth
+          </Typography>
+          </Grid>
+              <Grid item>            
+                <Typography type="subheading">secure, manage and exchange cryptocurrencies</Typography>
+              </Grid>
+              <Grid item>
+              <Button raised color="accent"
+                className={classes.button}
+                component={Link} to='/login'>
+                Start Trading
           </Button>
-      </Paper>
 
-      </Grid>
-      </Grid>
+
+              </Grid>
+            </Grid>
+          </Paper>
         </div>
+      </div>
       );
   }
 }

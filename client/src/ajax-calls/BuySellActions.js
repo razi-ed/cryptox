ec8f2983f410236cd8137b4b4ded057861f0f9e3
akshay upadhya <akshay.upadhya@mountblue.io>
2018-01-25 12:25:09 +0530

    fixing sell

--- client/src/ajax-calls/BuySellActions.js
+++ client/src/ajax-calls/BuySellActions.js
@@ -1,16 +1,25 @@
 export const bsAction =
-(action, state, mulFactor, props) => fetch(`/orders/${action}`, {
-    method: 'POST',
-    headers: {
-        'Authorization': localStorage.getItem('token'),
-        'Content-Type': 'application/json',
-    },
-    body: JSON.stringify({
-        units: ((state.units *
-            state.quantity) * (mulFactor(state.trade))).toFixed(3),
-        type: props.baseCurrency,
-        tradeFor: state.trade,
-        currentPrice: mulFactor(state.trade),
-    }),
-})
-.then(r=>r.json());
+(action, state, mulFactor, props) =>{
+    const type=(action=='buy')?props.baseCurrency:state.trade;
+    const tradeFor=(action=='buy')?state.trade:props.baseCurrency;
+    const currentPrice=(action=='buy')?mulFactor(state.trade):(1/mulFactor(state.trade));
+    const units=(action=='buy')?((state.units *
+        state.quantity) * (mulFactor(state.trade))).toFixed(3):((state.units *
+            state.quantity) ).toFixed(3);
+    console.log({type, tradeFor, currentPrice, units});
+
+    return fetch(`/orders/${action}`, {
+        method: 'POST',
+        headers: {
+            'Authorization': localStorage.getItem('token'),
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+            units,
+            type,
+            tradeFor,
+            currentPrice,
+        }),
+    })
+    .then(r=>r.json());
+};
